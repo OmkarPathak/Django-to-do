4d26a89004e8a392b3637a6f8c715e194ec97a66
Omkar Pathak <omkarpathak27@gmail.com>
2018-05-02 17:50:59 +0530

    Modified settings.py and added some basic urls for the tasks app

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('tasks/', include('tasks.urls')),
-    path('admin/', admin.site.urls),
+    path('add/', views.add, name='add_task'),
+    path('', views.test, name='test'),
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def test(request):
+    return render(request, 'base.html', {'name': 'root'})
+
+def add(request):
+    return render(request, 'base.html', {'name': 'add'})
