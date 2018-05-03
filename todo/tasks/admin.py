8f429e849bc28e2f934e51af644f6a06bc8bac51
Omkar Pathak <omkarpathak27@gmail.com>
2018-05-03 15:45:20 +0530

    Added some frontend changes and form manipulation

--- todo/tasks/admin.py
+++ todo/tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+class MyModelAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Task
+
+admin.site.register(Task, MyModelAdmin)
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
-from .models import TaskForm
+from .models import TaskForm, Task
 
 # Create your views here.
 def homepage(request):
     if request.method == 'POST':
-        pass
+        # this is wehere POST request is accessed
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        tasks = Task.objects.all()
+        return render(request, 'tasks.html', {'form': form, 'tasks': tasks})
     else:
+        # this is where GET request are accessed
         form = TaskForm()
-    return render(request, 'tasks.html', {'form': form})
+        tasks = Task.objects.all()
+    return render(request, 'tasks.html', {'form': form, 'tasks': tasks})
 
 def add(request):
     return render(request, 'tasks.html')
